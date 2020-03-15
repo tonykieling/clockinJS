2cd3c4e7e15200494768f6989d8bbdab526c416c
Tony Kieling <tony.kieling@gmail.com>
2020-03-15 11:43:17 -0700

    InvoiceEditModal FE almost done, missing validation of changed data + integration with backend. Plus, missing BE

--- front-end/src/component/InvoiceEditModal.js
+++ front-end/src/component/InvoiceEditModal.js
@@ -30,49 +30,243 @@ function InvoiceEditModal(props) {
     props.closeInvoiceEditModal();
   }
 
+  const [invoiceCode, setInvoiceCode] = useState(props.invoice.code);
+
+  const [receivedAmount, setReceivedAmount] = useState("");
+
+  const [reason, setReason] = useState("");
+
+  const [message, setMessage] = useState("");
+  
+  const [classNameMessage, setClassNameMessage] = useState("");
+  
+  const handleSubmit = async () => {
+    console.log("code- ", invoiceCode)
+    console.log("receivedAmount- ", receivedAmount)
+    console.log("reason- ", !(!reason || !reason.trim()))
+
+    if ((invoiceCode === props.invoice.code) && !receivedAmount && !!(reason || reason.trim())) {
+      setClassNameMessage("messageFailure");
+      setMessage("Nothing to be changed");
+    } else {
+      if (invoiceCode === "") {
+        setClassNameMessage("messageFailure");
+        setMessage("Missing Invoice's Code");
+      } else if ((receivedAmount !== "") && (reason === "" || !(reason.trim()))) {
+        setClassNameMessage("messageFailure");
+        setMessage("Missing Reason");
+      } else if ((receivedAmount === "") && ((reason !== "") || !!(reason.trim()))) {
+        setClassNameMessage("messageFailure");
+        setMessage("Missing Received Amount");
+      } else {
+        const data = {
+          code              : invoiceCode,
+          cad_adjustment    : receivedAmount,
+          reason_adjustment : reason
+        };
+        console.log("sending data", data);
+        // perform the action to send data to be recorded bu the server
+
+      }
+    }
+
+    clearMessage();
+  }
+
+
+  const clearMessage = () => {
+    setTimeout(() => {
+      setClassNameMessage("");
+      setMessage("");
+    }, 3000);
+  }
+
+
   return (
     <Modal
       show    = { showEditInvoice}
       onHide  = { handleClose }
     >
-      <Form>
-        { console.log("invoice:", props.invoice)}
-        <Form.Label style={{left: "2rem"}}> 
-          <b>Total: $</b>{ props.invoice.total_cad.toFixed(2) }
-        </Form.Label>
-        <br />
-        <Form.Label style={{left: "2rem"}}> 
-          <b>Date Start: </b> { show(props.invoice.date_start) }
-        </Form.Label>
-        <br />
-        <Form.Label> 
-          <b>Date End: </b> { show(props.invoice.date_end) }
-        </Form.Label>
-        <br />
-        <Form.Label style={{left: "2rem"}}> 
-          <b>Date Generated:</b> { show(props.invoice.date) }
-        </Form.Label>
+      {/* <Modal.Header closeButton={handleClose}>
+        <Modal.Title>Invoice {props.invoice.code}</Modal.Title>
+      </Modal.Header> */}
 
-        { (props.invoice.date_delivered || "this.state.dateDelivered") &&
-            <div>
-              <Form.Label style={{left: "2rem"}}> 
-                <b>Date Delivered:</b> { show(props.invoice.date_delivered || "this.state.dateDelivered") }
-              </Form.Label>
-            </div>
-        }
-        { (props.invoice.date_received || this.state.dateReceived) &&
-            <div>
-              <Form.Label style={{left: "2rem"}}> 
-                <b>Date Received:</b> { show(props.invoice.date_received || "this.state.dateReceived") }
-              </Form.Label>
-            </div>
-        }
-      </Form> 
-      <Button 
-        onClick = { handleClose }
-      >
-        Close
-      </Button>     
+      <Modal.Body>
+        <Card className="card-settings" style={{marginLeft: 0, backgroundColor: "lightsteelblue"}}>
+          <Card.Header>
+            Edit Invoice's data
+          </Card.Header>
+          <br />
+          <Form>
+            { console.log("invoice:", props.invoice)}
+
+            <Row>
+              <Col>
+                <Form.Label column className="cardLabel" style={{paddingLeft: "0px"}}>Total CAD</Form.Label>
+              </Col>
+              <Col>
+                <Form.Control
+                  style         = {{ textAlign: "right"}}
+                  disabled      = { true}
+                  value         = { `$ ${props.invoice.total_cad.toFixed(2)}`}
+                />
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                <Form.Label column className="cardLabel" style={{paddingLeft: "0px"}}>Date Start</Form.Label>
+              </Col>
+              <Col>
+                <Form.Control
+                  style         = {{ textAlign: "right"}}
+                  disabled      = { true}
+                  value         = { show(props.invoice.date_start) }
+                />
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                <Form.Label column className="cardLabel" style={{paddingLeft: "0px"}}>Date End</Form.Label>
+              </Col>
+              <Col>
+                <Form.Control
+                  style         = {{ textAlign: "right"}}
+                  disabled      = { true}
+                  value         = { show(props.invoice.date_end) }
+                />
+              </Col>
+            </Row>
+
+            <Row>
+              <Col>
+                <Form.Label column className="cardLabel" style={{paddingLeft: "0px"}}>Date Generated</Form.Label>
+              </Col>
+              <Col>
+                <Form.Control
+                  style         = {{ textAlign: "right"}}
+                  disabled      = { true}
+                  value         = { show(props.invoice.date) }
+                />
+              </Col>
+            </Row>
+
+            { props.invoice.date_delivered &&
+                <div>
+                  <Row>
+                    <Col>
+                      <Form.Label column className="cardLabel" style={{paddingLeft: "0px"}}>Date Delivered</Form.Label>
+                    </Col>
+                    <Col>
+                      <Form.Control
+                        style         = {{ textAlign: "right"}}
+                        disabled      = { true}
+                        value         = { show(props.invoice.date_delivered) }
+                      />
+                    </Col>
+                  </Row>
+                </div>
+            }
+
+            { props.invoice.date_received &&
+                <div>
+                  <Row>
+                    <Col>
+                      <Form.Label column className="cardLabel" style={{paddingLeft: "0px"}}>Date Received</Form.Label>
+                    </Col>
+                    <Col>
+                      <Form.Control
+                        style         = {{ textAlign: "right"}}
+                        disabled      = { true}
+                        value         = { show(props.invoice.date_received) }
+                      />
+                    </Col>
+                  </Row>
+                </div>
+            }
+
+            <br />
+            <Card>
+              <Card.Header>
+                What you can change
+              </Card.Header>
+              <Card.Body>
+                <Row>
+                  <Col>
+                    <Form.Label column className="cardLabel" style={{paddingLeft: "0px"}}>Code</Form.Label>
+                  </Col>
+                  <Col>
+                    <Form.Control
+                      style         = {{ textAlign: "right"}}
+                      type        = "text"
+                      placeholder = "Invoice's Code"
+                      name        = "invoiceCode"
+                      onChange    = { (event) => setInvoiceCode(event.target.value)}
+                      value       = { invoiceCode }
+                    />
+                  </Col>
+                </Row>
+
+                <Row>
+                  <Col>
+                    <Form.Label column className="cardLabel" style={{paddingLeft: "0px"}}>Received $</Form.Label>
+                  </Col>
+                  <Col>
+                    <Form.Control
+                      style         = {{ textAlign: "right"}}
+                      type        = "number"
+                      placeholder = "Actually $ received"
+                      name        = "receivedAmount"
+                      value       = { receivedAmount}
+                      onChange    = { (event) => Number(event.target.value) && setReceivedAmount(event.target.value)}
+                    />
+                  </Col>
+                </Row>
+
+                <Form.Label column className="cardLabel" style={{paddingLeft: "0px"}}>Reason</Form.Label>
+                <Form.Control
+                  style         = {{ textAlign: "right"}}
+                  as          = "textarea"
+                  rows        = "3"
+                  placeholder = "Why the diff btw the Total CAD and Received Amount"
+                  name        = "reason"
+                  value       = { reason}
+                  onChange    = { (event) => setReason(event.target.value)}
+                />
+
+              </Card.Body>
+            </Card>
+
+
+          </Form>
+
+          <br />
+          <Card.Footer className= { classNameMessage}>          
+            { message
+              ? message
+              : <br /> }
+          </Card.Footer>
+
+          <ButtonGroup className="mt-3">
+            <Button
+              style     = { { width: "50%" }}
+              onClick = { handleClose }
+            >
+              Close
+            </Button>
+            <Button
+              style     = { { width: "50%" }}
+              variant   = "success"
+              onClick   = { handleSubmit }
+            >
+              Save Changes
+            </Button>
+          </ButtonGroup>
+
+        </Card>
+      </Modal.Body>
+
     </Modal>
   )
 }
