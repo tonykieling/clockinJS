77807f658d3dbd4e64e3e80d4356e6bb5a01c4bd
Tony Kieling <tony.kieling@gmail.com>
2020-06-14 14:17:55 -0700

    deploying to heroku with SignUp rebuilded + invoice generator dates are empty and fixed the tip for invoice code after generating it.

--- front-end/src/component/SignUp.js
+++ front-end/src/component/SignUp.js
@@ -3,7 +3,6 @@ import Button   from 'react-bootstrap/Button';
 import Form     from 'react-bootstrap/Form';
 import Card     from 'react-bootstrap/Card';
 import { connect } from 'react-redux';
-import { Redirect } from 'react-router-dom';
 import axios from "axios";
 import MaskedInput from 'react-text-mask';
 import { findDOMNode } from "react-dom";
@@ -40,7 +39,6 @@ function SignUp(props) {
     content   : "",
     cssClass  : ""
   });
-  const [leave, setleave] = useState(false);
   const [emailValid, setemailValid] = useState(true);
   const [nameValid, setnameValid] = useState(true);
   const [changePC, setchangePC] = useState(false);
@@ -110,18 +108,20 @@ function SignUp(props) {
   }
 
 
+/*eslint-disable */
   useEffect(() => {
     if (changePC) {
       pcOutsideCanada ? refPC2.current.focus() : refPC1.current.focus();
       let count = 0;
       for (let x = (state.postalCode.length - 1); x >= 0; x-- ) {
-        if (state.postalCode[x] === "_" || state.postalCode[x] === " ") count++;
+        if (state.postalCode[x] === "_" || state.postalCode[x] === " ") 
+          count++;
         else break;
       }
       setstate({ ...state, postalCode: state.postalCode.slice(0, state.postalCode.length - count)})
-
     } 
   }, [pcOutsideCanada, setpcOutsideCanada]);
+/*eslint-enable */
 
 
   const handleSubmit = async e => {
@@ -144,7 +144,6 @@ function SignUp(props) {
         });
       } else {
         setdisableForm(true);
-
         const url = "/user/signup";
         const createUser  = {
           name        : state.name,
@@ -169,8 +168,8 @@ function SignUp(props) {
               postalCode  : addUser.data.user.postal_code,
               phone       : addUser.data.user.phone
             };
+            
             props.dispatchLogin({ user });
-            setleave(true);
           } else if (addUser.data.error) {
             console.log("it THROWS an ERROR")
             throw(addUser.data.error)
@@ -198,232 +197,230 @@ function SignUp(props) {
 
   return (
     <React.Fragment>
-      { leave && <Redirect to = "/" />}
-    <div className="formPosition">
-      <br />
-      <Card className="card-settings">
-        <Card.Header
-          ref = { refTop}
-        >
-          <h2>Sign Up</h2>
-        </Card.Header>
-        <Form 
-          onSubmit  = { handleSubmit} 
-          style     = {{ width: window.innerWidth < 800 || "70%", marginLeft: window.innerWidth < 800 || "15%"}}
-          autoComplete  = "nope"
-        >
+      <div className="formPosition">
+        <br />
+        <Card className="card-settings">
+          <Card.Header
+            ref = { refTop}
+          >
+            <h2>Sign Up</h2>
+          </Card.Header>
+          <Form 
+            onSubmit  = { handleSubmit} 
+            style     = {{ width: window.innerWidth < 800 || "70%", marginLeft: window.innerWidth < 800 || "15%"}}
+            autoComplete  = "nope"
+          >
+
+            <br />
+            <Form.Group controlId="formName">
+              <Form.Label className="cardLabel">Name</Form.Label>
+              <Form.Control
+                autoFocus   = {true}
+                type        = "text"
+                placeholder = "User's name"
+                name        = "name"
+                // onChange    = {e => handleChange(e)}
+                onChange    = { handleChange}
+                value       = {state.name}
+                disabled    = { disableForm}
+                // onKeyPress  = { e => handleChange(e)}
+                onKeyPress  = { handleChange}
+                onBlur      = { () => setnameValid(state.name ? true : false) }
+                ref         = { refName }
+              />
+              { !nameValid &&
+                  <Overlay
+                  target    = { refName}
+                  show      = {true}
+                  placement = "bottom"
+                  >
+                    <Tooltip id={"tooltip-bottom"}>
+                      <strong 
+                        style={{color: "gold"}}
+                        >
+                        Please, type a name
+                      </strong>
+                    </Tooltip>
+                  </Overlay>
+              }
+            </Form.Group>
+
+            <Form.Group 
+              controlId="formBasicEmail"
+              style       = {{marginBottom: "5px"}}
+            >
+              <Form.Label className="cardLabel">Email address</Form.Label>
+              <Form.Control
+                type        = "email"
+                placeholder = "User's email"
+                name        = "email"
+                onChange    = { handleChange}
+                onKeyPress  = { handleChange}
+                value       = {state.email}
+                disabled    = { disableForm}
+                onBlur      = { () => setemailValid(new RegExp(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g).test(state.email) ? true : false) }
+                ref         = { refEmail }
+              />
+            </Form.Group>
+            <Form.Text className="text-muted" style={{marginTop: "2px"}}>
+              We'll never share your email with anyone else.
+            </Form.Text>
+            <Form.Text className="text-muted" style={{marginTop: "0px", marginBottom: "16px"}}>
+              Set a real email so you will receive emails from Clockin.js.
+            </Form.Text>
+            { !emailValid &&
+                  <Overlay
+                  target    = { refEmail}
+                  show      = {true}
+                  placement = "bottom"
+                  >
+                    <Tooltip id={"tooltip-bottom"}>
+                      <strong 
+                        style={{color: "gold"}}
+                        >
+                        Please, type a valid email
+                      </strong>
+                    </Tooltip>
+                  </Overlay>
+              }
+            
+            <Form.Group controlId="formCity">
+              <Form.Label className="cardLabel">City</Form.Label>
+              <Form.Control
+                type        = "text"
+                placeholder = "Type the user's city"
+                name        = "city"
+                onChange    = { handleChange}
+                onKeyPress  = { handleChange}
+                value       = {state.city}
+                disabled    = { disableForm}
+                ref         = { refCity }
+                />
+            </Form.Group>
+
+            <Form.Group controlId="formAddress">
+              <Form.Label className="cardLabel">Address</Form.Label>
+              <Form.Control
+                type        = "text"
+                placeholder = "Type the user's address"
+                name        = "address"
+                onChange    = { handleChange}
+                onKeyPress  = { handleChange}
+                value       = {state.address}
+                disabled    = { disableForm}
+                ref         = { refAddress }
+                />
+            </Form.Group>
+
+            <Form.Group controlId="formPostalCode">
+              <Form.Label className="cardLabel">Postal Code</Form.Label>
+              <Form.Check 
+                inline 
+                label     = " outside Canada"
+                checked   = { pcOutsideCanada}
+                type      = "checkbox"
+                style     = {{marginLeft: "1rem"}}
+                disabled  = { disableForm}
+                onChange  = { onChangePCOutsideCanada}
+              />
+              { !pcOutsideCanada
+                ?
+                  <MaskedInput
+                    mask        = {[/[A-Z]/i, /\d/, /[A-Z]/i, ' ', /\d/, /[A-Z]/i, /\d/]}
+                    className   = "form-control"
+                    placeholder = "Enter client's postal code"
+                    name        = "postalCode"
+                    value       = {state.postalCode}
+                    disabled    = { disableForm}
+                    onChange    = { e => handlePostalCode(e)}
+                    onKeyPress  = { handleChange}
+                    ref         = {input => refPC1.current = findDOMNode(input) }
+                  />
+                : 
+                  <Form.Control
+                    type        = "text"
+                    placeholder = {"Enter client's postal code"}
+                    name        = "postalCode"
+                    onChange    = {e => handleChange(e)}
+                    value       = {state.postalCode}
+                    disabled    = { disableForm}
+                    onKeyPress  = { handleChange}
+                    ref         = { refPC2}
+                  />
+              }
+            </Form.Group>
+
+            <Form.Group controlId="formPhone">
+              <Form.Label className="cardLabel">Phone</Form.Label>
+              <MaskedInput
+                mask        = {['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+                className   = "form-control"
+                placeholder = "Enter your phone number"
+                name        = "phone"
+                id          = "phone"
+                value       = {state.phone}
+                onKeyPress  = {e => handleChange(e)}
+                disabled    = { disableForm}
+                ref         = { input => refPhone.current = findDOMNode(input) }
+                />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label className="cardLabel">Password</Form.Label>
+              <Form.Control
+                type        = "password"
+                placeholder = "Password"
+                name        = "password"
+                onChange    = { handleChange}
+                value       = { state.password}
+                disabled    = { disableForm}
+                onKeyPress  = { handleChange}
+                ref         = { refPassword }
+                autoComplete = "on"
+                />
+            </Form.Group>
+
+            <Form.Group controlId="formConfirmPassword">
+              <Form.Label className="cardLabel">Confirm Password</Form.Label>
+              <Form.Control
+                type        = "password"
+                placeholder = "Confirm Password"
+                name        = "confirmPassword"
+                onChange    = { handleChange}
+                value       = {state.confirmPassword}
+                disabled    = { disableForm}
+                onKeyPress  = { handleChange}
+                ref         = { refConfirmPassword }
+                autoComplete = "off"
+              />
+            </Form.Group>
+
+
+          <Card.Footer className= { message.cssClass}>          
+            { message.content
+              ? message.content
+              : <br /> }
+          </Card.Footer>
 
           <br />
-          <Form.Group controlId="formName">
-            <Form.Label className="cardLabel">Name</Form.Label>
-            <Form.Control
-              autoFocus   = {true}
-              type        = "text"
-              placeholder = "User's name"
-              name        = "name"
-              // onChange    = {e => handleChange(e)}
-              onChange    = { handleChange}
-              value       = {state.name}
-              disabled    = { disableForm}
-              // onKeyPress  = { e => handleChange(e)}
-              onKeyPress  = { handleChange}
-              onBlur      = { () => setnameValid(state.name ? true : false) }
-              ref         = { refName }
-            />
-            { !nameValid &&
-                <Overlay
-                target    = { refName}
-                show      = {true}
-                placement = "bottom"
-                >
-                  <Tooltip id={"tooltip-bottom"}>
-                    <strong 
-                      style={{color: "gold"}}
-                      >
-                      Please, type a name
-                    </strong>
-                  </Tooltip>
-                </Overlay>
-            }
-          </Form.Group>
-
-          <Form.Group 
-            controlId="formBasicEmail"
-            style       = {{marginBottom: "5px"}}
-          >
-            <Form.Label className="cardLabel">Email address</Form.Label>
-            <Form.Control
-              type        = "email"
-              placeholder = "User's email"
-              name        = "email"
-              onChange    = { handleChange}
-              onKeyPress  = { handleChange}
-              value       = {state.email}
-              disabled    = { disableForm}
-              onBlur      = { () => setemailValid(new RegExp(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g).test(state.email) ? true : false) }
-              ref         = { refEmail }
-            />
-          </Form.Group>
-          <Form.Text className="text-muted" style={{marginTop: "2px"}}>
-            We'll never share your email with anyone else.
-          </Form.Text>
-          <Form.Text className="text-muted" style={{marginTop: "0px", marginBottom: "16px"}}>
-            Set a real email so you will receive emails from Clockin.js.
-          </Form.Text>
-          { !emailValid &&
-                <Overlay
-                target    = { refEmail}
-                show      = {true}
-                placement = "bottom"
-                >
-                  <Tooltip id={"tooltip-bottom"}>
-                    <strong 
-                      style={{color: "gold"}}
-                      >
-                      Please, type a valid email
-                    </strong>
-                  </Tooltip>
-                </Overlay>
-            }
-          
-          <Form.Group controlId="formCity">
-            <Form.Label className="cardLabel">City</Form.Label>
-            <Form.Control
-              type        = "text"
-              placeholder = "Type the user's city"
-              name        = "city"
-              onChange    = { handleChange}
-              onKeyPress  = { handleChange}
-              value       = {state.city}
-              disabled    = { disableForm}
-              ref         = { refCity }
-              />
-          </Form.Group>
-
-          <Form.Group controlId="formAddress">
-            <Form.Label className="cardLabel">Address</Form.Label>
-            <Form.Control
-              type        = "text"
-              placeholder = "Type the user's address"
-              name        = "address"
-              onChange    = { handleChange}
-              onKeyPress  = { handleChange}
-              value       = {state.address}
-              disabled    = { disableForm}
-              ref         = { refAddress }
-              />
-          </Form.Group>
-
-          <Form.Group controlId="formPostalCode">
-            <Form.Label className="cardLabel">Postal Code</Form.Label>
-            <Form.Check 
-              inline 
-              label     = " outside Canada"
-              checked   = { pcOutsideCanada}
-              // onClick   = { () => console.log("PC CLick")}
-              // onChange  = { () => console.log("PC Change")}
-              type      = "checkbox"
-              style     = {{marginLeft: "1rem"}}
+          <div className="d-flex flex-column">
+            <Button 
+              variant   = "primary" 
+              type      = "submit"
+              onClick   = { handleSubmit}      
               disabled  = { disableForm}
-              onChange  = { onChangePCOutsideCanada}
-            />
-            { !pcOutsideCanada
-              ?
-                <MaskedInput
-                  mask        = {[/[A-Z]/i, /\d/, /[A-Z]/i, ' ', /\d/, /[A-Z]/i, /\d/]}
-                  className   = "form-control"
-                  placeholder = "Enter client's postal code"
-                  name        = "postalCode"
-                  value       = {state.postalCode}
-                  disabled    = { disableForm}
-                  onChange    = { e => handlePostalCode(e)}
-                  onKeyPress  = { handleChange}
-                  ref         = {input => refPC1.current = findDOMNode(input) }
-                />
-              : 
-                <Form.Control
-                  type        = "text"
-                  placeholder = {"Enter client's postal code"}
-                  name        = "postalCode"
-                  onChange    = {e => handleChange(e)}
-                  value       = {state.postalCode}
-                  disabled    = { disableForm}
-                  onKeyPress  = { handleChange}
-                  ref         = { refPC2}
-                />
-            }
-          </Form.Group>
+              ref       = { refButtonSubmit}
+            >
+              Submit
+            </Button>
+          </div>
 
-          <Form.Group controlId="formPhone">
-            <Form.Label className="cardLabel">Phone</Form.Label>
-            <MaskedInput
-              mask        = {['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
-              className   = "form-control"
-              placeholder = "Enter your phone number"
-              name        = "phone"
-              id          = "phone"
-              value       = {state.phone}
-              onKeyPress  = {e => handleChange(e)}
-              disabled    = { disableForm}
-              ref         = { input => refPhone.current = findDOMNode(input) }
-              />
-          </Form.Group>
-
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label className="cardLabel">Password</Form.Label>
-            <Form.Control
-              type        = "password"
-              placeholder = "Password"
-              name        = "password"
-              onChange    = { handleChange}
-              value       = { state.password}
-              disabled    = { disableForm}
-              onKeyPress  = { handleChange}
-              ref         = { refPassword }
-              />
-          </Form.Group>
-
-          <Form.Group controlId="formConfirmPassword">
-            <Form.Label className="cardLabel">Confirm Password</Form.Label>
-            <Form.Control
-              type        = "password"
-              placeholder = "Confirm Password"
-              name        = "confirmPassword"
-              onChange    = { handleChange}
-              value       = {state.confirmPassword}
-              disabled    = { disableForm}
-              onKeyPress  = { handleChange}
-              ref         = { refConfirmPassword }
-            />
-          </Form.Group>
-
-
-        <Card.Footer className= { message.cssClass}>          
-          { message.content
-            ? message.content
-            : <br /> }
-        </Card.Footer>
-
-        <br />
-        <div className="d-flex flex-column">
-          <Button 
-            variant   = "primary" 
-            type      = "submit"
-            onClick   = { handleSubmit}      
-            disabled  = { disableForm}
-            ref       = { refButtonSubmit}
-          >
-            Submit
-          </Button>
-        </div>
-
-        </Form>
-      </Card>
-    <br></br>
-    <br></br>
-  </div>
-  </React.Fragment>
-
+          </Form>
+        </Card>
+        <br></br>
+        <br></br>
+      </div>
+    </React.Fragment>
   )
 }
 
