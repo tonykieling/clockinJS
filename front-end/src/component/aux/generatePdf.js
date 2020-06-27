511f0f6cc0eb608f9671fbf2e84a2b8188082bdd
Tony Kieling <tony.kieling@gmail.com>
2020-06-27 10:06:21 -0700

    invoice for BC Autism Program was modified in order to allow multiple pages. Done

--- front-end/src/component/aux/generatePdf.js
+++ front-end/src/component/aux/generatePdf.js
@@ -2,7 +2,6 @@ import axios from "axios";
 import jsPDF from 'jspdf';
 import { show } from "./formatDate.js";
 
-
 const getClocinks = async (userToken, clientId, dateStart, dateEnd) => {
   const url = `/clockin?dateStart=${dateStart}&dateEnd=${dateEnd}&clientId=${clientId}`;
 
@@ -29,6 +28,10 @@ const generatePdf = async(props) => {
                                props.client._id, 
                                props.invoice.date_start.substring(0, 10), 
                                props.invoice.date_end.substring(0, 10));
+
+  const pageNumbers     = Math.ceil(clockins.length / 14);
+  let currentPageNumber = 1;
+  
   const data = {
     invoiceDate         : props.invoice.date,
     invoiceNumber       : props.invoice.code,
@@ -41,43 +44,95 @@ const generatePdf = async(props) => {
     phoneNumber3        : props.user.phone.substring(10,14),
     client              : props.client.name,
     typeOfService       : "Behaviour Intervention",
-    clockins,
     totalCad            : props.invoice.total_cad,
     invoiceSample       : props.client.invoice_sample
   };
   
   let doc = new jsPDF();
 
-  doc.addImage(data.invoiceSample, "JPEG", 0, 0, 210, 297);
+  do {
+    if (currentPageNumber > 1) doc.addPage();
+    doc.addImage(data.invoiceSample, "JPEG", 0, 0, 210, 297);
 
-  doc.setTextColor(92, 76, 76);
-  doc.setFontSize(12);
-  
-  doc.text(show(data.invoiceDate), 131, 30);
-  doc.text(data.invoiceNumber, 168, 30);
-  doc.text(data.serviceProviderName, 77, 41);
-  doc.text(data.mailingAddres, 65, 51.5);
-  doc.text(data.city, 42, 61.5);
-  doc.text(data.postalCode, 154, 61.5);
-  doc.text(data.phoneNumber1, 63, 71.5);
-  doc.text(data.phoneNumber2, 80, 71.5);
-  doc.text(data.phoneNumber3, 91.5, 71.5);
-  doc.text(data.client, 72, 164);
-  
-  doc.setFontSize(11);
-  doc.text(data.typeOfService, 30, 190);
-  data.clockins.forEach((e, i) => doc.text(show(e.date), 78, ((i * 4.25) + 190)));
-  data.clockins.forEach((e, i) => doc.text(`${(e.worked_hours / 3600000).toFixed(2)}`, 110, ((i * 4.25) + 190)));
-  data.clockins.forEach((e, i) => doc.text(`${(e.rate).toFixed(2)}`, 135, ((i * 4.25) + 190)));
-  data.clockins.forEach((e, i) => doc.text(`${((e.worked_hours / 3600000) * e.rate).toFixed(2)}`, 188, ((i * 4.25) + 190), {align: "right"}));
+    doc.setTextColor(92, 76, 76);
+    doc.setFontSize(12);
+    
+    doc.text(show(data.invoiceDate), 131, 30);
+    doc.text(data.invoiceNumber, 168, 30);
+    doc.text(data.serviceProviderName, 77, 41);
+    doc.text(data.mailingAddres, 65, 51.5);
+    doc.text(data.city, 42, 61.5);
+    doc.text(data.postalCode, 154, 61.5);
+    doc.text(data.phoneNumber1, 63, 71.5);
+    doc.text(data.phoneNumber2, 80, 71.5);
+    doc.text(data.phoneNumber3, 91.5, 71.5);
+    doc.text(data.client, 72, 164);
+    
+    doc.setFontSize(11);
+    doc.text(data.typeOfService, 30, 190);
+    
 
-  const array = data.clockins.map(e => Number(((e.worked_hours)/3600000).toFixed(2)));
-  const totalHoursWorked = array.reduce((accumulator, currentValue) => accumulator + currentValue);
-  const totalCadToReceive = (data.clockins[0].rate * totalHoursWorked).toFixed(2);
-  doc.text(totalCadToReceive, 188, 250.4, {align: "right"});
-  doc.text("---", 179, 255.5);
-  doc.text(totalCadToReceive, 188, 261, {align: "right"});
+    if (pageNumbers < 2) {
+      // it prints dates
+      clockins.forEach((e, i) => doc.text(show(e.date), 78, ((i * 4.25) + 190)));
+      // it prints # of hours
+      clockins.forEach((e, i) => doc.text(`${(e.worked_hours / 3600000).toFixed(2)}`, 120, ((i * 4.25) + 190), {align: "right"}));
+      // it prints Rate per Hour
+      clockins.forEach((e, i) => doc.text(`${(e.rate).toFixed(2)}`, 150, ((i * 4.25) + 190), {align: "right"}));
+      // it prints Total Amount
+      clockins.forEach((e, i) => doc.text(`${((e.worked_hours / 3600000) * e.rate).toFixed(2)}`, 189.5, ((i * 4.25) + 190), {align: "right"}));
+    } else { // it sets for printing the clockins in more than one page
+      const clockinsTemp = clockins.slice(((currentPageNumber - 1) * 14), (currentPageNumber * 14));
+      // it prints dates
+      clockinsTemp.forEach((e, i) => doc.text(show(e.date), 78, ((i * 4.25) + 190)));
+      // it prints # of hours
+      clockinsTemp.forEach((e, i) => doc.text(`${(e.worked_hours / 3600000).toFixed(2)}`, 120, ((i * 4.25) + 190), {align: "right"}));
+      // it prints Rate per Hour
+      clockinsTemp.forEach((e, i) => doc.text(`${(e.rate).toFixed(2)}`, 150, ((i * 4.25) + 190), {align: "right"}));
+      // it prints Total Amount
+      clockinsTemp.forEach((e, i) => doc.text(`${((e.worked_hours / 3600000) * e.rate).toFixed(2)}`, 189.5, ((i * 4.25) + 190), {align: "right"}));
+    }
 
+    const array = clockins.map(e => Number(((e.worked_hours)/3600000).toFixed(2)));
+    const totalHoursWorked = array.reduce((accumulator, currentValue) => accumulator + currentValue);
+    const totalCadToReceive = (clockins[0].rate * totalHoursWorked).toFixed(2);
+
+    let pageLabel = "",
+        totalLabel = "";
+    if (pageNumbers > 1) { 
+      doc.setFontSize(9);
+      switch (pageNumbers) {
+        case 2:
+          pageLabel   = `Page ${currentPageNumber} of 2`;
+          doc.setFontType("italic");
+          totalLabel  = "see 2nd page";
+          break;
+        case 3:
+          pageLabel   = `Page ${currentPageNumber} of 3`;
+          doc.setFontType("italic");
+          totalLabel = "see 3rd page";
+          break;
+        default:
+          pageLabel   = `Page ${currentPageNumber} of ${pageNumbers}`;
+          doc.setFontType("italic");
+          totalLabel = `see ${pageNumbers}th page`;
+          break;
+      }
+    }
+
+    const flagPosition = (pageNumbers > 1 && currentPageNumber < pageNumbers) ? true : false;
+    !flagPosition && doc.setFontSize(12);
+    doc.text( (flagPosition ? totalLabel : totalCadToReceive), (flagPosition ? 191 : 189.5), 250.4, {align: "right"});
+    doc.text("---", 179, 255.5);
+    doc.text((flagPosition ? totalLabel : totalCadToReceive), (flagPosition ? 191 : 189.5), 261, {align: "right"});
+    
+    
+    doc.setFontSize(12);
+    doc.setFontType("normal");
+    doc.text(pageLabel, 191, 289, {align: "right"});
+
+    currentPageNumber++;
+  } while(currentPageNumber <= pageNumbers)
   doc.save(`${data.invoiceNumber}.pdf`);
   
   
