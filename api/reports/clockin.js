822de1f837f2791e8561d3c40b0b7dc5e1e503af
Tony Kieling <tony.kieling@gmail.com>
2020-09-06 11:49:35 -0700

    first BE version for clockin is done. Next step is to have a FE version and integrate with the first one.

--- api/reports/clockin.js
+++ api/reports/clockin.js
@@ -1,9 +1,10 @@
+"use strict";
+
 const mongoose  = require("mongoose");
 
 const Clockin   = require("../models/clockin.js");
 const Client    = require("../models/client.js");
-const user = require("../models/user.js");
-const { cat } = require("shelljs");
+// const user = require("../models/user.js");
 
 // mongoose.set("debug", true);
 
@@ -17,7 +18,7 @@ const general = async(req, res) => {
   // }
   // const userId    = req.userData.userId;
   const userId  = "5db0d13c35d7c5475beb17fd";
-
+// console.trace("======> req.body", req.body)
   const 
     clientId  = req.body.clientId,
     dateStart = new Date(req.body.dateStart),
@@ -29,100 +30,80 @@ const general = async(req, res) => {
     return res.json({error: "Error: Report 02"});
   }
 
-// console.log("dateEnd", dateStart)
-let clockins = "";
+  // console.log("dateEnd", dateStart)
+  let result = {};
   if (all) {
     // calls the function for all clients
-    clockins = await allClients(userId, clientId, dateStart, dateEnd);
+    const summary = await queryClockins(userId, dateStart, dateEnd)
+    const clients = await allClients(userId);
+    const arrayOfClockinsByClient = await clients.map(async e => 
+      await queryClockins(userId, dateStart, dateEnd, e._id, (e.nickname || e.name))  
+    );
+    const clockinsByClient = await Promise.all(arrayOfClockinsByClient);
+
+    result = {
+      summary,
+      clockinsByClient
+    };
+
   } else {
     // calls the function for a specific client
-    clockins = await specificClient(userId, clientId, dateStart, dateEnd);
-    console.log("XXXXXXXXXXXXXclockins", clockins, "\n\n")
+    const summary = await queryClockins(userId, dateStart, dateEnd, clientId);
+    result.summary = summary;
   }
 
-  const totalHours = (clockins.allClockins.reduce((a, b) => {
-    return b.worked_hours 
-      ? (a + b.worked_hours) 
-      : (a + (new Date(b.time_end) - new Date(b.time_start)))
-  }, 0)) / 3600000;
-
-  const invoicedClockins = clockins.allClockins.filter(e => e.invoice_id);
-  console.log("invoicedClockins", invoicedClockins)
-
-  const totalClockinsInvoiced = invoicedClockins.length;
-  const totalHoursInvoiced    = (invoicedClockins.reduce((a, b) => 
-      b.worked_hours
-        ? (a + b.worked_hours)
-        : (a + (new Date(b.time_end) - new Date(b.time_start))), 0) / 3600000);
-
-  const totalClockinsNoInvoice  = clockins.allClockins.length - totalClockinsInvoiced;
-  const totalHoursNoInvoice     = totalHours - totalHoursInvoiced;
-  
-  const result = all 
-    ?
-      {
-        message: "TEMPPPPPPPPPPPPPPP"
-      }
-    : {
-        period: {
-          dateStart,
-          dateEnd
-        },
-        totalClockins : clockins.count,
-        totalClockinsInvoiced,
-        totalClockinsNoInvoice,
-        totalHours,
-        totalHoursInvoiced,
-        totalHoursNoInvoice,
-        client: clockins.client
-      }
+  result = {
+    ...result,
+    period: {
+      dateStart,
+      dateEnd
+    }
+  };
 
   return res.json(result);
 
 }
 
 
-//comment
-const specificClient = async (userId, clientId, dateStart, dateEnd) => {
-  console.log("inside specific client")
-
-  let allClockins = "";
-  try { 
-    allClockins = await Clockin
-      .find( 
-        {
-          user_id: mongoose.Types.ObjectId(userId),
-          date: {
-            $gte: dateStart,
-            $lte: dateEnd
-          },
-          client_id: mongoose.Types.ObjectId(clientId)
-        }
-      )
-      .sort({date: 1});
-
-    console.log("===> allClockins", allClockins)
-  
-    if (!allClockins || allClockins.length < 1) {
-      return ({
-        error: `No clockins at all.`
+//it gets info about the clients
+const allClients = async(userId) => {
+  try {
+    // checks client info
+    const clients = await Client
+      .find({
+        user_id: userId
       });
-    }
+
+    return(clients);
   } catch (error) {
     console.log("Error: Clockin Report - 03 ");
     return ({
       error: "Error: Clockin Report - 03 "
     });
   }
+}
 
 
 
-  let client = "";
+//it queries the dbs the clockins for either a user or a client
+const queryClockins = async (userId, dateStart, dateEnd, clientId, clientName) => {
+  const conditions = {
+    user_id: mongoose.Types.ObjectId(userId),
+    date: {
+      $gte: dateStart,
+      $lte: dateEnd
+    }
+  }
+  // how to ignore (not consider) a field in find mongodb
+  if (clientId) conditions.client_id = mongoose.Types.ObjectId(clientId);
+
+
+  let allClockins = "";
   try {
-    // checks client info
-    client = await Client
-      .findById( clientId )
-      .select(" nickname ");
+    allClockins = await Clockin
+      .find(conditions)
+      .sort({date: 1})
+
   } catch (error) {
     console.log("Error: Clockin Report - 04 ");
     return ({
@@ -130,141 +111,60 @@ const specificClient = async (userId, clientId, dateStart, dateEnd) => {
     });
   }
 
-  return ({
-    count: allClockins.length,
-    allClockins,
-    client: client.nickname || client.name
-  });
-
-}
 
 
+  let client = "";
+  if (!clientName && clientId) {
+    try {
+      // checks client info
+      client = await Client
+        .findById( clientId );
 
-
-const allClients = async(userId, dateStart, dateEnd) => {
-  return({message: "TESSSSSSSSSSSSST"});
-};
-
-
-
-
-
-const all = async (req, res) => {
-  console.log("inside clockins' report - specific");
-
-  const userId    = req.userData.userId;
-
-  const 
-    clientId  = req.query.clientId,
-    dateStart = new Date(req.query.dateStart || "2000-03-01T09:00:00.000Z"),
-    dateEnd   = new Date(req.query.dateEnd || "2100-03-01T09:00:00.000Z");
-
-  try { 
-    let allClockins = null;
-    // if (!userAdmin)
-    if (userAdmin) /////////////// ----->>>>> this is the right one
-      allClockins = await Clockin
-        .find()
-        .select(" date time_start time_end rate notes invoice_id client_id user_id ");
-    else {
-      // https://stackoverflow.com/questions/6502541/mongodb-query-multiple-collections-at-once
-      // this code works - BEFORE doing a $lookup
-      // allClockins = await Clockin
-        // .find({ 
-        //   user_id: userId,
-        //   date: {
-        //     $gte: dateStart,
-        //     $lte: dateEnd
-        //   },
-        //   client_id: clientId
-        // })
-        // .select(" date time_start time_end rate notes invoice_id client_id user_id ");
-      allClockins = await Clockin
-        .aggregate([
-          { $match: 
-            {
-              user_id: mongoose.Types.ObjectId(userId),
-              date: {
-                $gte: dateStart,
-                $lte: dateEnd
-              },
-              client_id: mongoose.Types.ObjectId(clientId)
-            }
-          },
-          { $lookup: 
-            {
-              from: "invoices",
-              localField: "invoice_id",
-              foreignField: "_id",
-              as: "invoice"
-            }
-          },
-          {
-            $unwind: {
-              path :'$invoice', 
-              preserveNullAndEmptyArrays: true
-            }
-          },
-          {
-              $project: {
-                  "invoice.__v": 0,
-                  // "invoice._id": 0,
-                  "invoice.date": 0,
-                  "invoice.date_start": 0,
-                  "invoice.date_end": 0,
-                  "invoice.notes": 0,
-                  "invoice.status": 0,
-                  "invoice.total_cad": 0,
-                  "invoice.client_id": 0,
-                  "invoice.user_id": 0
-              }
-          }
-        ])
-        .sort({date: 1});
-    }
-
-    if (!allClockins || allClockins.length < 1) {
-      return res.status(200).json({
-        error: `No clockins at all.`
+    } catch (error) {
+      console.log("Error: Clockin Report - 05 ");
+      return ({
+        error: "Error: Clockin Report - 05 "
       });
     }
-
-
-    /**
-     * it checks whether the application is querying for last invoice code (queryLastInvoiceCode) used
-     * if so, it calls checkInvoiceCode, which returns either:
-     *  - null
-     *  - the last code used (when the code is not ended with a valid number)
-     *  - a new code, adding one to its ending part - here, it is an object with a attribute newCode
-     *  */
-    // const codeSuggestion = allClockins.length && await checkInvoiceCode(allClockins);
-    const codeSuggestion = req.query.queryLastInvoiceCode ? await checkInvoiceCode(userId, clientId) : null;
-
-    if (clientId) {
-      const client = await Client
-        .findById( clientId )
-        .select(" nickname ");
-
-
-      return res.status(200).json({
-        count: allClockins.length,
-        allClockins,
-        client: client.nickname, 
-        codeSuggestion
-      });
-    };
-
-    return res.status(200).json({
-      count: allClockins.length,
-      allClockins
-    });
-  
-  } catch(err) {
-    console.log("Error => ", err.message);
-    return res.status(422).json({
-      error: "EACK02: Something's got wrong."
-    });
   }
+
+  if (!allClockins || allClockins.length < 1)
+    return ({
+      message: `No clockins at all.`,
+      client: client ? client.nickname || client.name : clientName
+    });
+
+
+  const totalHours = (allClockins.reduce((a, b) => {
+    return b.worked_hours 
+      ? (a + b.worked_hours) 
+      : (a + (new Date(b.time_end) - new Date(b.time_start)))
+  }, 0)) / 3600000;
+
+  const invoicedClockins = allClockins.filter(e => e.invoice_id);
+
+  const totalClockinsInvoiced = invoicedClockins.length;
+  const totalHoursInvoiced    = (invoicedClockins.reduce((a, b) => 
+      b.worked_hours
+        ? (a + b.worked_hours)
+        : (a + (new Date(b.time_end) - new Date(b.time_start))), 0) / 3600000);
+
+  const totalClockinsNoInvoice  = allClockins.length - totalClockinsInvoiced;
+  const totalHoursNoInvoice     = totalHours - totalHoursInvoiced;
+
+  const result = {
+    totalClockins : allClockins.length,
+    totalHours,
+    totalClockinsInvoiced,
+    totalHoursInvoiced,
+    totalClockinsNoInvoice,
+    totalHoursNoInvoice
+  }
+  if (client || clientName) 
+    result.client = client ? (client.nickname || client.name) : clientName;
+
+  return (result);
+
 }
 
 
